25e218be8fe2c6cca9d7afad9367146ca2d79aaf
Anukul <sspl-lap-064@Admins-MacBook-Pro-3.local>
2022-10-19 15:24:56 +0530

    append added

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,8 @@ public class Main {
         myLinkedList.getHead();
         myLinkedList.getTail();
         myLinkedList.getLength();
+//        myLinkedList.printList();
+        myLinkedList.append(8);
         myLinkedList.printList();
 
 
