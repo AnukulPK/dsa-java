4f23404a88b60c0d0f4a772a736cf739a3b4be02
Anukul <sspl-lap-064@Admins-MacBook-Pro-3.local>
2022-11-10 16:01:44 +0530

    set

--- src/LinkedListCustom.java
+++ src/LinkedListCustom.java
@@ -117,4 +117,14 @@ public class LinkedListCustom {
         return temp;
 
     }
+
+    public boolean set(int index, int value){
+        Node temp=get(index);
+
+        if(temp!=null ){
+            temp.value=value;
+            return true;
+        }
+        return false;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
-    public static void printItems(int n){
-        for(int i=0;i<n;i++ ){
-            for(int j=0;j<n;j++ ){
-                System.out.println(i+" "+j);
+    public static void printItems(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(i + " " + j);
             }
         }
 
@@ -19,23 +19,25 @@ public class Main {
 //        System.out.println(cookieOne.getColor());
 //        System.out.println(cookieTwo.getColor());
 
-        LinkedListCustom myLinkedList = new LinkedListCustom(2);
+        LinkedListCustom myLinkedList = new LinkedListCustom(11);
 //        myLinkedList.getHead();
 //        myLinkedList.getTail();
 //        myLinkedList.getLength();
 //        myLinkedList.printList();
 //        myLinkedList.append(8);
         myLinkedList.append(3);
-        myLinkedList.prepend(1);
+        myLinkedList.append(23);
+        myLinkedList.append(7);
+        myLinkedList.set(1,4);
 
 //        myLinkedList.printList();
 //        System.out.println(myLinkedList.removeFirst().value);
 //        System.out.println(myLinkedList.removeFirst().value);
 //        System.out.println(myLinkedList.removeFirst().value);
 //        System.out.println(myLinkedList.removeFirst());
-        System.out.println(myLinkedList.get(2).value);
+//        System.out.println(myLinkedList.get(2).value);
 
-
+        myLinkedList.printList();
 
     }
 }
