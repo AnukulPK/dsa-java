5244b7d756b0d7e5327e70b857f5117232289252
unknown <anukulpk2@gmail.com>
2023-01-02 14:36:29 +0530

    bst constructor

---
+++ src/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,16 @@
+package BinarySearchTree;
+
+public class BinarySearchTree {
+    Node root;
+
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+}
