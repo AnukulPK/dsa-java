41482a342bfa6e524f25f26abd174c80f238dfb2
unknown <anukulpk2@gmail.com>
2023-02-08 10:31:08 +0530

    main updated for hashtable

--- src/hashtable/Main.java
+++ src/hashtable/Main.java
@@ -1,4 +1,11 @@
 package hashtable;
 
 public class Main {
+    public static void main(String[] args) {
+
+        HashTable myHashTable = new HashTable();
+        myHashTable.printTable();
+    }
+
+
 }
