e9380a100174a445a8e3df5ca592dc6aec61aa1e
unknown <anukulpk2@gmail.com>
2023-02-10 10:39:24 +0530

    set in hashtable

--- src/hashtable/Main.java
+++ src/hashtable/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
         HashTable myHashTable = new HashTable();
+        myHashTable.set("nails",100);
+        myHashTable.set("tiles",100);
+        myHashTable.set("lumber",100);
+
+        myHashTable.set("bolts",100);
+        myHashTable.set("screws",100);
         myHashTable.printTable();
     }
 
